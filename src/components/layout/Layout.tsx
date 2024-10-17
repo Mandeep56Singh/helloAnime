28cfb4bdb0a20105de0b397d54099ca05954bbba
mandeepSingh <mandeepraj2312@gmail.com>
2024-10-17 19:42:11 +0530

    feat:add layout for all pages

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,20 +1,12 @@
 import Box from "@mui/material/Box";
+import { Outlet } from "react-router-dom";
+import { Navbar } from "../navbar/Navbar";
+
 const Layout = () => {
   return (
-    <Box
-      sx={{
-        backgroundColor: "#201F31",
-        color: "#fff",
-        display: "flex",
-        flexDirection: {
-          xs: "column",
-          lg: "row",
-        },
-        padding: 3,
-        overflowX: "hidden",
-      }}
-    >
-      Layout
+    <Box>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
     </Box>
   );
 };
