3bf35a8a54377b17d49ecb998f9988d3c48f9ec0
mandeepSingh <mandeepraj2312@gmail.com>
2024-10-24 20:45:53 +0530

    refactor: delete commented code and useless files

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,22 +1,12 @@
-
 import { Box } from "@mui/material";
 import SpotLightSlder from "../components/SpotLight/SpotLightSlider";
 
 const HomePage = () => {
-  // const { loading, error, data } = useQuery(GetAnimePage, {
-  //   variables: { PerPage: 30, Page: 1 },
-  // });
-  // if (error) return <p>Error : {error.message}</p>;
-
-  // if (loading) return <p>Loading...</p>;
-  // const animeList = data?.Page?.media;
- return (
-  <Box>
-
-    <SpotLightSlder></SpotLightSlder>
-  </Box>
-
- )
+  return (
+    <Box>
+      <SpotLightSlder></SpotLightSlder>
+    </Box>
+  );
 };
 
 export default HomePage;
