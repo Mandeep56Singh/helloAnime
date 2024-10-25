7c9c7eeb39e54e7268b7c5a3ef445faa6d2ec80f
mandeepSingh <mandeepraj2312@gmail.com>
2024-10-26 00:21:57 +0530

    fix: resolve z-Index issue of searchbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -21,7 +21,13 @@ export const Navbar = () => {
     setSearchToggle(false);
   },[location])
   return (
-    <Box>
+    <Box sx={{
+      position:'absolute',
+      width:"100%",
+      top:0,
+      left:0,
+      zIndex:99
+    }}>
       <AppBar
         position={"sticky"}
         aria-label="navbar"
